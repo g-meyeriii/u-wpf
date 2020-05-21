945d5d779cd291163035760e7eaad5d2febe3714
gjmiii9@gmail.com <gjmiii9@gmail.com>
2020-05-21 10:42:42 -0700

    soccer scores app listbox

--- Wpf01/Wpf01/MainWindow.xaml.cs
+++ Wpf01/Wpf01/MainWindow.xaml.cs
@@ -28,15 +28,22 @@ namespace Wpf01
         //public static readonly DependencyProperty myDependencyProperty =
         //    DependencyProperty.Register("MyProperty", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
 
-            public Sum SumObject { get; set; }
+        //public Sum SumObject { get; set; }
 
         public MainWindow()
         {
 
             InitializeComponent();
+            List<Match> matches = new List<Match>();
+            matches.Add(new Match() { Team1 = "Bayern Munich  ", Team2 = "  Real Madrid", Score1 = 3, Score2 = 2, Completion = 85});
+            matches.Add(new Match() { Team1 = "PSG  ", Team2 = "  Barca", Score1 = 3, Score2 = 2, Completion = 55 });
+            matches.Add(new Match() { Team1 = "BVB Dortmund  ", Team2 = "  AS Roma", Score1 = 3, Score2 = 2, Completion = 25 });
+            matches.Add(new Match() { Team1 = "Man United  ", Team2 = "  LA Galaxy", Score1 = 1, Score2 = 1, Completion = 35 });
 
-            SumObject = new Sum { Num1 = "1", Num2 = "3" };
-            this.DataContext = SumObject;
+            lbMatches.ItemsSource = matches;
+
+            //SumObject = new Sum { Num1 = "1", Num2 = "3" };
+            //this.DataContext = SumObject;
 
             
             
@@ -110,5 +117,29 @@ namespace Wpf01
 
             //}
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(lbMatches.SelectedItem != null)
+            {
+                MessageBox.Show("Selected Match: "
+                    + (lbMatches.SelectedItem as Match).Team1 + " " +
+                     (lbMatches.SelectedItem as Match).Score1 + " " +
+                     (lbMatches.SelectedItem as Match).Score2 + " " +
+                     (lbMatches.SelectedItem as Match).Team2 
+                     );
+
+            }
+
+        }
     }
+    public class Match
+    {
+        public int Score1 { get; set; }
+        public int Score2 { get; set; }
+        public string Team1 { get; set; }
+        public string Team2 { get; set; }
+        public int Completion { get; set; }
+    }
+
 }
